b77aa4137ed7b305940e58960b5fad959a950f57
jonandresen <jon.eirik@lisle.no>
2023-10-15 20:57:39 +0200

    fixed stuff

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { ListSection } from "./ListSection";
+
+export default function Home() {
+  return (
+    <main>
+      <ListSection />
+    </main>
+  );
+}
