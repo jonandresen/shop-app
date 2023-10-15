f99e1fa37c9a8c7cdd36aa25b05de95724388d11
jonandresen <jon.eirik@lisle.no>
2023-10-15 16:31:34 +0200

    Added simple list component with input and submit field

---
+++ src/ListSection.tsx
@@ -0,0 +1,42 @@
+import {
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
+  Button,
+  Input,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
+export const ListSection = () => {
+  const [textInputValue, setTextInputValue] = useState("");
+  const [shoppingList, setShoppingList] = useState<string[]>([]);
+
+  const handleSubmit = () => {
+    setShoppingList([textInputValue, ...shoppingList]);
+    setTextInputValue("");
+  };
+
+  return (
+    <>
+      <List>
+        {shoppingList.map(l => {
+            return (
+            <ListItem>
+                {l}
+            </ListItem>
+            )
+        })}
+      </List>
+
+      <Input
+        value={textInputValue}
+        onInput={(e) => {
+          setTextInputValue((e.target as HTMLInputElement).value);
+        }}
+      ></Input>
+      <Button onClick={handleSubmit}>Legg til!</Button>
+    </>
+  );
+};
