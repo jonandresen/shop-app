ae13c36ed110ba703ed0ce465d3332d88ccf0c2e
jonandresen <jon.eirik@lisle.no>
2023-10-21 22:03:15 +0200

    Made some silly edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Heading, VStack } from "@chakra-ui/react";
 export default function Home() {
   return (
     <main>
-      <VStack width={'100%'} >
+      <VStack width={'100%'} height={'100%'} >
         <Heading as={"h1"}>Handleliste</Heading>
         <ListSection />
       </VStack>
