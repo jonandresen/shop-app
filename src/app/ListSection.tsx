fa3861588f2ed4129192544b2953fdc10479015d
jonandresen <jon.eirik@lisle.no>
2023-10-21 21:39:21 +0200

    Use local storage to make it stick

--- src/app/ListSection.tsx
+++ src/app/ListSection.tsx
@@ -9,24 +9,78 @@ import {
   Button,
   Input,
   VStack,
+  Box,
+  IconButton,
+  HStack,
 } from "@chakra-ui/react";
 import React from "react";
 import { useState } from "react";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+
+const LIST_DATA_LS_KEY = "LIST_DATA_LS_KEY";
 
 export const ListSection = () => {
+  const initialData = localStorage.getItem(LIST_DATA_LS_KEY);
   const [textInputValue, setTextInputValue] = useState("");
-  const [shoppingList, setShoppingList] = useState<string[]>([]);
+  const [shoppingList, setShoppingList] = useState<string[]>(
+    initialData ? JSON.parse(initialData) : []
+  );
+
+  const addToList = (listValue: string) => {
+    localStorage.setItem(
+      LIST_DATA_LS_KEY,
+      JSON.stringify([
+        ...new Set([listValue.toLocaleUpperCase(), ...shoppingList]),
+      ])
+    );
+    const localData = localStorage.getItem(LIST_DATA_LS_KEY);
+    setShoppingList(localData ? JSON.parse(localData) : []);
+  };
+
+  const valueIsStored = (value: string): boolean => {
+    const storedData = localStorage.getItem(LIST_DATA_LS_KEY);
+    if (storedData === null) return false;
+    const parsedData = JSON.parse(storedData) as string[];
+    return !!parsedData.find(
+      (i) => i.toLocaleUpperCase() === value.toLocaleUpperCase()
+    );
+  };
 
   const handleSubmit = () => {
-    setShoppingList([...new Set([textInputValue, ...shoppingList])]);
+    addToList(textInputValue);
     setTextInputValue("");
+  };
+
+  const handleRemoveButton = (item: string) => {
+    localStorage.setItem(
+      LIST_DATA_LS_KEY,
+      JSON.stringify([
+        ...shoppingList.filter(
+          (li) => li.toLocaleUpperCase() !== item.toLocaleUpperCase()
+        ),
+      ])
+    );
+    const localData = localStorage.getItem(LIST_DATA_LS_KEY);
+    setShoppingList(localData ? JSON.parse(localData) : []);
   };
 
   return (
     <VStack>
       <List>
         {shoppingList.map((l) => {
-          return <ListItem>{l}</ListItem>;
+          return (
+            <ListItem>
+              <HStack>
+                <Box>{l}</Box>
+                <IconButton
+                  variant={"ghost"}
+                  icon={<AiOutlineCloseCircle />}
+                  aria-label={"remove-button"}
+                  onClick={() => handleRemoveButton(l)}
+                />
+              </HStack>
+            </ListItem>
+          );
         })}
       </List>
 
